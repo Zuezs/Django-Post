4893222a4ee0823f4a054d1e44bb6c8d637f422c
tallen <tyron_d_allen@homedepot.com>
2023-10-11 17:06:17 -0500

    Django fixes

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,10 @@ class Post(models.Model):
     body = models.TextField()
     slug = models.SlugField(null=False, unique=True, max_length=200) 
     created_at = models.DateTimeField(auto_now_add=True)
+    is_draft = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+    
+    class Meta:
+        ordering = ('-created_at', )
--- blog/tests.py
+++ blog/tests.py
@@ -5,10 +5,28 @@ from blog.models import Post
 
 
 class PostListViewTests(TestCase):
+    def test_drafts_hidden(self):
+        draft_post = Post.objects.create(
+            title="A title", body="Blog content.", slug="test-slug", is_draft=True
+        )
+
+        # Test that we start with our post in a draft status
+        self.assertTrue(draft_post.is_draft)
+
+        url = reverse("posts")
+        response = self.client.get(url)
+        self.assertEqual(200, response.status_code)
+
+        # Special Django Helper Method
+        self.assertQuerysetEqual(response.context["post_list"], [])
+
     def test_posts_visible(self):
         post = Post.objects.create(
             title="A title", body="Blog content.", slug="test-slug"
         )
+
+        # Test that we start with our post in draft status
+        self.assertFalse(post.is_draft)
 
         url = reverse("posts")
         response = self.client.get(url)
@@ -19,3 +37,14 @@ class PostListViewTests(TestCase):
 
         # Test the webpage content contains our post body.
         self.assertContains(response, "Blog content.")
+
+class PostDetailViewTests(TestCase):
+    def test_draft_post_returns_404(self):
+
+        draft_post = Post.objects.create(
+            title="A title", body="Blog content.", slug="test-slug", is_draft=True
+        )
+
+        url = reverse("post", args=(draft_post.slug, ))
+        response = self.client.get(url)
+        self.assertEqual(404, response.status_code)
